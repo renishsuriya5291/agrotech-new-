1ba77e0ad6c44344bd9ff105697d83bbe0b86414
lyricalboii <lyricalboii001@gmail.com>
2022-08-31 20:57:02 +0530

    commit

--- db/config.php
+++ db/config.php
@@ -6,8 +6,8 @@ session_start();
 // init configuration
 $clientID = '837007103942-1m0uhlp66bgcd5k22jff1pa0cftbc94d.apps.googleusercontent.com';
 $clientSecret = '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';
-$redirectUri = 'https://aggrotech.herokuapp.com/index.php';
-// $redirectUri = 'http://localhost/agrotech-new-/index.php';
+// $redirectUri = 'https://aggrotech.herokuapp.com/index.php';
+$redirectUri = 'http://localhost/agrotech-new-/index.php';
 
 // create Client Request to access Google API
 $client = new Google_Client();
